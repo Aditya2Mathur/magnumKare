e9c023516a461af8e2968e2fd89e9b1590e3471d
Aditya2Mathur <adiasfreelancer@gmail.com>
2024-04-13 08:36:32 +0530

    form reset after 3.5s

--- assets/js/form.js
+++ assets/js/form.js
@@ -10,14 +10,13 @@
       })
       .then(response => {
         if (response.ok) {
-          document.getElementById('alertMessage').innerText = alert("Message sent successfully");
           document.getElementById('alertMessage').innerText = "Message sent successfully";
           document.getElementById('alertMessage').style.display = "block";
           // Reset the form after 5 seconds
           setTimeout(function() {
-            document.getElementById('MakeYourBooking').reset();
-            document.getElementById('alertMessage').innerText = "Thank you for your submission!";
-          }, 5000); // 5000 milliseconds = 5 seconds
+            document.getElementById('contact-with-us').reset();
+            document.getElementById('alertMessage').innerText = "Thank you for Contant with me";
+          }, 3500); // 5000 milliseconds = 3.5 seconds
         } else {
           document.getElementById('alertMessage').innerText = "Failed to send message";
           document.getElementById('alertMessage').style.display = "block";
